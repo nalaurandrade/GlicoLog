634441339eb7ed8f374971c5e383391a6c66b2cd
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2026-04-29 15:48:15 -0300

    ajuste

--- src/screens/calendario.js
+++ src/screens/calendario.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { View, Text } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
-import Calendario from './src/screens/calendario';
-import Grafico from './src/screens/grafico';
-
-const Drawer = createDrawerNavigator();
-
-export default function App() {
+export default function Calendario() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Calendário">
-
-        <Drawer.Screen name="Calendário" component={Calendario} />
-        <Drawer.Screen name="Gráfico" component={Grafico} />
-
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Calendário</Text>
+      <Calendar />
+    </View>
   );
 }
